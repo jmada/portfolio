ee659c38de76d483140ffc5fb5b32b5cc8c0c065
Juan Paredes <juanmadpb@gmail.com>
2020-12-23 01:18:13 -0300

    Add about page. Add general text.

---
+++ src/components/About.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div id="wrapper">
+      <div id="main">
+        <section id="one">
+          <div class="image main" data-position="center">
+            <img src="images/banner.jpg" alt="" />
+          </div>
+          <div class="container">
+            <header class="major">
+              <h2>Hello there!</h2>
+              <p>
+                My name is Juan Paredes and
+                <br />
+                I'm a Full Stack Developer.
+              </p>
+            </header>
+            <p>
+              I like working on Web Development Projects, I have 4 years
+              experience working as Web Developer, creating landing pages,
+              administration systems (web based), API REST, e-Commerce and other
+              projects that involves Web Development. For fun I love traveling,
+              reading and working on amazing projects.
+            </p>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
