3c014e80bcba46a8d8bb1894690ea31b0a2a6a53
Juan Paredes <juanmadpb@gmail.com>
2020-12-26 15:10:56 -0300

    Update style file.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./assets/css/main.css";
+import "./assets/sass/main.scss";
 import routes from "./routes/index";
 import Header from "./components/Header";
 
