a7ef5deacb1be72a2aafa3be9be52181f88f5b0c
Juan Paredes <juanmadpb@gmail.com>
2020-12-28 16:06:47 -0300

    Add ScrollToTop component.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/sass/main.scss";
 import routes from "./routes/index";
+import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Header />
       <Switch>
