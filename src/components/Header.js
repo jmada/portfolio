d6bd2bfd254b1aa2fffa5cb12fcc23de90da7356
Juan Paredes <juanmadpb@gmail.com>
2020-12-28 20:05:18 -0300

    Add social links env variables.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+const {
+  REACT_APP_PERSONAL_EMAIL,
+  REACT_APP_GITHUB_URL,
+  REACT_APP_LINKEDIN_URL,
+  REACT_APP_RESUME_URL,
+} = process.env;
+
 export default function Header() {
   const [isWindowsSizeMediumOrLess, setIsWindowsSizeMediumOrLess] = useState(
     false
@@ -65,25 +72,40 @@ export default function Header() {
       <footer>
         <ul className="icons">
           <li>
-            <a href="#" className="icon">
+            <a href={`mailto:${REACT_APP_PERSONAL_EMAIL}`} className="icon">
               <FontAwesomeIcon icon={faEnvelope} />
               <span className="label">Email</span>
             </a>
           </li>
           <li>
-            <a href="#" className="icon">
+            <a
+              href={`${REACT_APP_GITHUB_URL}`}
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={faGithub} />
               <span className="label">Github</span>
             </a>
           </li>
           <li>
-            <a href="#" className="icon">
+            <a
+              href={`${REACT_APP_LINKEDIN_URL}`}
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={faLinkedin} />
               <span className="label">LinkedIn</span>
             </a>
           </li>
           <li>
-            <a href="#" className="icon">
+            <a
+              href={`${REACT_APP_RESUME_URL}/export?format=pdf`}
+              className="icon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <FontAwesomeIcon icon={faDownload} />
               <span className="label">Download Resume</span>
             </a>
