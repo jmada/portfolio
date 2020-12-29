81dca461c0a59945a2bdc75ae137d9370a64d1ba
Juan Paredes <juanmadpb@gmail.com>
2020-12-29 19:53:20 -0300

    Update routes directory.

---
+++ src/utils/routes/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import AboutPage from "../../pages/About";
+import SkillPage from "../../pages/Skill";
+import ProjectPage from "../../pages/Project";
+import ContactPage from "../../pages/Contact";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    component: () => <AboutPage />,
+    protected: null,
+  },
+  {
+    path: "/skills",
+    exact: true,
+    component: () => <SkillPage />,
+    protected: null,
+  },
+  {
+    path: "/projects",
+    exact: true,
+    component: () => <ProjectPage />,
+    protected: null,
+  },
+  {
+    path: "/contact",
+    exact: true,
+    component: () => <ContactPage />,
+    protected: null,
+  },
+];
+
+export default routes;
