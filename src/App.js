9b7af1084fa595e88ff3dad80c7eed0ce666140c
Juan Paredes <juanmadpb@gmail.com>
2020-12-29 17:20:46 -0300

    Update routes directory.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/sass/main.scss";
-import routes from "./routes/index";
+import routes from "./utils/routes/index";
 import ScrollToTop from "./components/ScrollToTop";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
