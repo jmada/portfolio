e1548d254d5e7aae2f713535397ad74a15e5dff3
Juan Paredes <juanmadpb@gmail.com>
2021-01-02 20:32:12 -0300

    Import SocialIcon component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faDownload } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-
-const {
-  REACT_APP_PERSONAL_EMAIL,
-  REACT_APP_GITHUB_URL,
-  REACT_APP_LINKEDIN_URL,
-  REACT_APP_RESUME_URL,
-} = process.env;
+import SocialIcon from "./SocialIcon";
 
 export default function Header() {
   const [isWindowsSizeMediumOrLess, setIsWindowsSizeMediumOrLess] = useState(
@@ -74,51 +65,7 @@ export default function Header() {
         </ul>
       </nav>
       <footer>
-        <ul className="icons">
-          <li>
-            <a
-              href={`mailto:${REACT_APP_PERSONAL_EMAIL}`}
-              className="icon"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faEnvelope} />
-              <span className="label">Email</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href={`${REACT_APP_GITHUB_URL}`}
-              className="icon"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faGithub} />
-              <span className="label">Github</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href={`${REACT_APP_LINKEDIN_URL}`}
-              className="icon"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-              <span className="label">LinkedIn</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href={`${REACT_APP_RESUME_URL}/export?format=pdf`}
-              className="icon"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={faDownload} />
-              <span className="label">Download Resume</span>
-            </a>
-          </li>
-        </ul>
+        <SocialIcon />
       </footer>
     </section>
   );
