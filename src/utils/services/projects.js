2750d61faab7ee59f9ab08bd591735bf5cc57587
Juan Paredes <juanmadpb@gmail.com>
2021-01-02 23:22:23 -0300

    Add projects service.

---
+++ src/utils/services/projects.js
@@ -0,0 +1,106 @@
+const projects = [
+  {
+    id: 1,
+    link: {
+      href: "https://aisforartisan.org/",
+    },
+    image: {
+      src: "/projects/a_is_for_artisan_home.png",
+      alt: "A is for Artisan - Home Page",
+    },
+    title: "A is for Artisan",
+    description:
+      "A is for Artisan is a non profit organization based in Washington DC that works to celebrate and promote Latin American craft.",
+    technologies: [
+      {
+        id: 1,
+        name: "Semantic HTML5",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+      },
+      {
+        id: 3,
+        name: "Bulma CSS Framework",
+      },
+      {
+        id: 4,
+        name: "Gulp",
+      },
+      {
+        id: 5,
+        name: "WordPress",
+      },
+      {
+        id: 6,
+        name: "PHP",
+      },
+    ],
+    summary:
+      "WordPress was requested by the client. He wanted the ability to change, add and remove content on his site without the need of a developer.",
+  },
+  {
+    id: 2,
+    link: {
+      href: "https://rplogistics.com.py/",
+    },
+    image: {
+      src: "/projects/rplogistics_home.png",
+      alt: "RPLogistics - Home Page",
+    },
+    title: "RPLogistics",
+    description:
+      "RPLogistics is a company based in Paraguay that provices logistics services. Also provices rent of forklift, cranes and freight services.",
+    technologies: [
+      {
+        id: 1,
+        name: "WordPress",
+      },
+      {
+        id: 2,
+        name: "PHP",
+      },
+    ],
+    summary:
+      "WordPress was requested by the client. He wanted the ability to change, add and remove content on his site without the need of a developer.",
+  },
+  {
+    id: 3,
+    link: {
+      href: "https://jmparedes.me/",
+    },
+    image: {
+      src: "/projects/personal_website_home.png",
+      alt: "Juan Paredes's Portfolio - Home Page",
+    },
+    title: "Juan Paredes's Portfolio",
+    description: "This is my first personal website (Portfolio).",
+    technologies: [
+      {
+        id: 1,
+        name: "Semantic HTML5",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+      },
+      {
+        id: 3,
+        name: "SASS",
+      },
+      {
+        id: 4,
+        name: "ReactJS",
+      },
+      {
+        id: 5,
+        name: "JavaScript",
+      },
+    ],
+    summary:
+      "ReactJS was my first option. I was really interested in built it in ReactJS.",
+  },
+];
+
+export default projects;
