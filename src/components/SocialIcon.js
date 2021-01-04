e4c2ea7ad76be96bfea4ede49600ad20be81fd8d
Juan Paredes <juanmadpb@gmail.com>
2021-01-04 18:01:11 -0300

    Add Tooltip to icons.

--- src/components/SocialIcon.js
+++ src/components/SocialIcon.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Tooltip from "./Tooltip";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faDownload } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -14,47 +15,55 @@ export default function SocialIcon() {
   return (
     <ul className="icons">
       <li>
-        <a
-          href={`mailto:${REACT_APP_PERSONAL_EMAIL}`}
-          className="icon"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faEnvelope} />
-          <span className="label">Email</span>
-        </a>
+        <Tooltip tip="Email">
+          <a
+            href={`mailto:${REACT_APP_PERSONAL_EMAIL}`}
+            className="icon"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span className="label">Email</span>
+          </a>
+        </Tooltip>
       </li>
       <li>
-        <a
-          href={`${REACT_APP_GITHUB_URL}`}
-          className="icon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faGithub} />
-          <span className="label">Github</span>
-        </a>
+        <Tooltip tip="Github">
+          <a
+            href={`${REACT_APP_GITHUB_URL}`}
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            <span className="label">Github</span>
+          </a>
+        </Tooltip>
       </li>
       <li>
-        <a
-          href={`${REACT_APP_LINKEDIN_URL}`}
-          className="icon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faLinkedin} />
-          <span className="label">LinkedIn</span>
-        </a>
+        <Tooltip tip="LinkedIn">
+          <a
+            href={`${REACT_APP_LINKEDIN_URL}`}
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+            <span className="label">LinkedIn</span>
+          </a>
+        </Tooltip>
       </li>
       <li>
-        <a
-          href={`${REACT_APP_RESUME_URL}/export?format=pdf`}
-          className="icon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faDownload} />
-          <span className="label">Download Resume</span>
-        </a>
+        <Tooltip tip="Resume">
+          <a
+            href={`${REACT_APP_RESUME_URL}/export?format=pdf`}
+            className="icon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faDownload} />
+            <span className="label">Download Resume</span>
+          </a>
+        </Tooltip>
       </li>
     </ul>
   );
